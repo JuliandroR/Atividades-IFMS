9df7fcabfba185c12e0f534a043b67e45aed0adf
Juliandro Rocha <juliandro.ribeiro@novaandradina.org>
2018-12-06 11:56:08 -0300

    Incremento do calculo de consumo

--- Fisica/scripts/index.js
+++ Fisica/scripts/index.js
@@ -1,34 +1,72 @@
 let valorKhw = 0;
 let potencia = 0;
 let validaty = true;
+let result_final = 0.0
+let tempo_utilizado = 0;
 
 function definirValor(){
     valorKhw = parseFloat(document.getElementById('type_immobile').value);
-    console.log(valorKhw);
+    if(valorKhw == 0.54500){
+        adicionaClassInvisible('type_item_industrial')
+        adicionaClassInvisible('type_item_rural')
+        adicionaClassInvisible('input_pot')
+        removeClassInvisible('type_item_home');
+    }else if(valorKhw == 0.38151){
+        adicionaClassInvisible('type_item_home')
+        adicionaClassInvisible('type_item_industrial')
+        removeClassInvisible('type_item_rural');
+        adicionaClassInvisible('input_pot')
+    }else if(valorKhw == 0.54501){
+        adicionaClassInvisible('type_item_home')
+        adicionaClassInvisible('input_pot')
+        adicionaClassInvisible('type_item_rural')
+        removeClassInvisible('type_item_industrial');
+    }else{}
 }
 
-function definirPotencia(){
-    value = document.getElementById('type_item').value;
-
-    if(value != "000"){
-        potencia = parseFloat(value);
-        validaty = true;
-    }else{
-        removeClassInvisible('input_pot', 'invisible');
+function definirPotencia(elemento){
+    let pot = (document.getElementById(elemento).value);
+    if(pot == "000"){
+        removeClassInvisible('input_pot');
         validaty = false;
+    }else{
+        potencia = parseFloat(pot);    
     }
-};
-
-function removeClassInvisible(elemento, classe){
-    var element = document.getElementById(elemento);
-    element.classList.remove(classe);
 }
 
 var button = document.querySelector('#calcular');
 
 button.addEventListener('click', function () {
-    if(validaty == false){
-        potencia = parseFloat(document.getElementById('time_used').value)
+    tempo_utilizado = parseFloat(document.getElementById('time_used').value);
+    if (validaty == false) {
+        potencia = parseFloat(document.getElementById('input_pot').value)
     }
-    alert(`O valor a ser pago com as informações fornecidas é: ${(potencia/100)*valorKhw}`);
+    let result = ((potencia*tempo_utilizado)*valorKhw);
+    result_final += parseFloat(result.toFixed(2));
+
+    retornaHtml(potencia, tempo_utilizado, result);
+    altera_total(result_final);
 });
+
+
+function removeClassInvisible(elemento) {
+    var element = document.getElementById(elemento);
+    element.classList.remove("invisible");
+}
+
+function adicionaClassInvisible(elemento){
+    var element = document.getElementById(elemento);
+    element.classList.add("invisible");
+}
+
+function retornaHtml(potencia, tempo, custo) {
+    var pNova = document.createElement('p');
+    var addc = document.createTextNode(`A potência do produto em KhW é: ${potencia}. Foi utilizado por: ${tempo} horas. O custo foi: R$ ${custo}`);
+    pNova.appendChild(addc);
+    var div = document.getElementById('retorno')
+    div.appendChild(pNova)
+}
+
+function altera_total(valor){
+    document.getElementById('resultado').innerHTML = parseFloat(valor.toFixed(2));
+}
